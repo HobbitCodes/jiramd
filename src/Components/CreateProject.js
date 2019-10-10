952da1964fcbb0c78e75f643200a934dbda0b119
Harry Wiseman <harry@strawberrysoup.co.uk>
2019-10-10 07:42:01 +0100

    Workingon the react form

--- src/Components/CreateProject.js
+++ src/Components/CreateProject.js
@@ -1,27 +1,53 @@
 import React, {Component} from 'react';
 import Title from "./Title";
+import axios from 'axios';
+
+const API_PATH = 'http://jiramdapp.strawberrystage.co.uk/api/jira/index.php';
 
 class CreateProject extends Component {
 
     constructor() {
         super();
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleFileUpload = this.handleFileUpload.bind(this);
+
+        this.state = {
+            md_files: ''
+        };
     }
 
+    handleFileUpload(event) {
+        event.preventDefault();
+
+        console.table(event.target.files);
+
+        this.setState({
+            md_files: event.target.files
+        });
+    }
 
     handleSubmit(event) {
         event.preventDefault();
-        const imageLink = event.target.elements.link.value;
-        const description = event.target.elements.description.value;
-        const post = {
-            id: Number(new Date()),
-            description: description,
-            imageLink: imageLink
-        }
+        const key = event.target.project_key.value;
+        const user = event.target.user.value;
+        const password = event.target.password.value;
+        const host = event.target.host.value;
 
-        if (description && imageLink) {
-            this.props.onAddPhoto(post)
-        }
+        let formData = new FormData();
+        formData.append('project_key', key);
+        formData.append('project_user', user);
+        formData.append('project_password', password);
+        formData.append('project_host', host);
+
+        this.state.md_files.map((file) => {
+            // make diffent formData per each file and request.
+            formData.append('myFile', file);
+        });
+
+        axios.post(`${API_PATH}`, formData)
+            .then(response => {
+                console.log(response);
+            }).catch(error => (error.response));
     }
 
     render() {
@@ -29,9 +55,12 @@ class CreateProject extends Component {
             <div>
                 <Title title={'Jira MD'}/>
                 <div className="form">
-                    <form onSubmit={this.handleSubmit}>
+                    <form className="box" onSubmit={this.handleSubmit}>
+                        <input type="text" name="jira_host" placeholder="https://{COMPANY_NAME}.atlassian.net"/>
+                        <input type="text" name="jira_user" placeholder="Username"/>
+                        <input type="password" name="jira_password" placeholder="***********"/>
                         <input type="text" name="project_key" placeholder="HWBI"/>
-                        <input type="text" name="project_directory" placeholder="/Users/user/Sites/docs/."/>
+                        <input type="file" name="files[]" id="file" multiple onChange={this.handleFileUpload}/>
                         <button>Create Project</button>
                     </form>
                 </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import Title from './Title';
-import Photowall from './Photowall';
 import CreateProject from './CreateProject';
 import {Link, Route} from 'react-router-dom';
+//import axios from 'axios';
 
 // url("https://image.flaticon.com/icons/svg/60/60740.svg") center no-repeat;
 
@@ -12,70 +12,52 @@ class Main extends Component {
         super();
         // Adding it to the state means when it updates, it will
         // re trigger the render function
+        // axios.get('https://api.github.com/repos/HobbitCodes/jiramd/issues/?client_id=Iv1.591334f2fdada2a4&client_secret=448cfb4b95778b26fed90fb3f646748322ac7f08')
+        //     .then(resp => {
+        //         console.table(resp.data);
+        //     });
+
         this.state = {
-            posts: [
-                {
-                    id: 0,
-                    description: "beautiful landscape",
-                    imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-                        "3919321_1443393332_n.jpg"
-                },
-                {
-                    id: 1,
-                    description: "Aliens???",
-                    imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-                        "3919321_1443393332_n.jpg"
-                },
-                {
-                    id: 2,
-                    description: "On a vacation!",
-                    imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-                }
-            ],
             screen: 'photos'
         };
-
-        // Where remove photo is a function it loses access to this
-        // So by doing this we bind it to it
-        this.removePhoto = this.removePhoto.bind(this);
-    }
-
-    // Update state by removing photo selected
-    removePhoto(postRemoved) {
-        // Get current state, return only the posts that !== the postRemoved
-        this.setState((state) => ({
-            posts: state.posts.filter(post => post !== postRemoved)
-        }))
-    }
-
-    addPhoto(postSubmitted) {
-        this.setState(state => ({
-            posts: state.posts.concat([postSubmitted])
-        }));
-    }
-
-    // Envoked immediately after component added to DOM - Use for API calls
-    componentDidMount() {
-        //const posts = SimulateFetchFromDatabase();
-
-        // If there is no current state no need to fetch it
-        //this.setState({
-        //  posts: posts
-        //})
     }
 
     render() {
-        console.table(this.state.posts);
         return (
-            <section>
+            <section className="container">
                 <Route exact path="/" render={() => (
-                    <div>
-                        <Title title={'Jira MD'}/>
-                        <div>
-                            <Link className="addIcon" to="/CreateProject"></Link>
-                        </div>
+                    <div className="page-content">
+                        <section>
+                            <Title title={'Jira MD'}/>
+                            <div className="intro-section">
+                                <div className="intro-section__copy">
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et enim eleifend,
+                                        eleifend nulla et, ullamcorper arcu. Aenean massa orci, lobortis non nunc vel,
+                                        facilisis congue risus. In condimentum porta felis sit amet laoreet. Nunc sodales
+                                        sapien vel euismod viverra. Nullam felis dolor, semper ut maximus quis, vehicula sit
+                                        amet massa.
+                                    </p>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et enim eleifend,
+                                        eleifend nulla et, ullamcorper arcu. Aenean massa orci, lobortis non nunc vel,
+                                        facilisis congue risus. In condimentum porta felis sit amet laoreet. Nunc sodales
+                                        sapien vel euismod viverra. Nullam felis dolor, semper ut maximus quis, vehicula sit
+                                        amet massa.
+                                    </p>
+                                </div>
+                                <Link className="button" to="/CreateProject">Get Started</Link>
+                            </div>
+                        </section>
+                        <section>
+                            <Title title={'Known Issues'}/>
+                            <div className="intro-section">
+                                <a className="button" href="https://github.com/HobbitCodes" target="_blank" rel="noopener noreferrer">View repo</a>
+                            </div>
+                        </section>
                     </div>
                 )}/>
+
                 <Route path="/CreateProject" render={({history}) => (
                     <CreateProject onAddPhoto={(addedPost) => {
                         this.addPhoto(addedPost);
