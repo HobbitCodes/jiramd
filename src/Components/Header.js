20b261d68f5c65c4586c27ffcaa4e8180be11b27
Harry Wiseman <harry@strawberrysoup.co.uk>
2019-10-11 08:00:56 +0100

    Menu into a component

---
+++ src/Components/Header.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+
+        this.state = {
+            menu_class: 'site-header__nav main-menu',
+            hamburger_class: 'hamburger',
+        };
+    }
+
+    onClick(e) {
+        e.preventDefault();
+        this.setState({menu_class: (this.state.menu_class === 'site-header__nav main-menu') ? 'site-header__nav site-header__nav--active main-menu' : 'site-header__nav main-menu'});
+        this.setState({hamburger_class: (this.state.hamburger_class === 'hamburger') ? 'hamburger hamburger--active' : 'hamburger'});
+    }
+
+    render() {
+        return (
+            <header className="site-header">
+                <a href="/">Site logo</a>
+                <div className={this.state.hamburger_class} onClick={this.onClick}>
+                    <span className="hamburger__top"></span>
+                    <span className="hamburger__middle"></span>
+                    <span className="hamburger__bottom"></span>
+                </div>
+                <div className={this.state.menu_class}>
+                    <ul>
+                        <li><a>Get Started</a></li>
+                        <li><a href="https://github.com/HobbitCodes" target="_blank" rel="noopener noreferrer">View repo</a></li>
+                    </ul>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import Title from './Title';
-import CreateProject from './CreateProject';
+import Header from './Header';
 import {Link, Route} from 'react-router-dom';
+import JiraForm from "./JiraForm";
 //import axios from 'axios';
 
 // url("https://image.flaticon.com/icons/svg/60/60740.svg") center no-repeat;
@@ -10,12 +11,6 @@ class Main extends Component {
     constructor() {
         //Same as parent();
         super();
-        // Adding it to the state means when it updates, it will
-        // re trigger the render function
-        // axios.get('https://api.github.com/repos/HobbitCodes/jiramd/issues/?client_id=Iv1.591334f2fdada2a4&client_secret=448cfb4b95778b26fed90fb3f646748322ac7f08')
-        //     .then(resp => {
-        //         console.table(resp.data);
-        //     });
 
         this.state = {
             screen: 'photos'
@@ -24,47 +19,45 @@ class Main extends Component {
 
     render() {
         return (
-            <section className="container">
+            <div className="container">
                 <Route exact path="/" render={() => (
                     <div className="page-content">
+                        <Header/>
                         <section>
-                            <Title title={'Jira MD'}/>
+                            <div className="form">
+                                <JiraForm/>
+                            </div>
+                        </section>
+                        <section>
+                            <Title title={'What is Jira MD?'}/>
                             <div className="intro-section">
                                 <div className="intro-section__copy">
                                     <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et enim eleifend,
-                                        eleifend nulla et, ullamcorper arcu. Aenean massa orci, lobortis non nunc vel,
-                                        facilisis congue risus. In condimentum porta felis sit amet laoreet. Nunc sodales
-                                        sapien vel euismod viverra. Nullam felis dolor, semper ut maximus quis, vehicula sit
-                                        amet massa.
+                                        Jira MD is a tool that used markdown files to create your Jira board.
                                     </p>
                                     <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et enim eleifend,
-                                        eleifend nulla et, ullamcorper arcu. Aenean massa orci, lobortis non nunc vel,
-                                        facilisis congue risus. In condimentum porta felis sit amet laoreet. Nunc sodales
-                                        sapien vel euismod viverra. Nullam felis dolor, semper ut maximus quis, vehicula sit
-                                        amet massa.
+                                        It does this by targeting specific markdown syntax within the functional document.
                                     </p>
                                 </div>
-                                <Link className="button" to="/CreateProject">Get Started</Link>
                             </div>
                         </section>
                         <section>
                             <Title title={'Known Issues'}/>
                             <div className="intro-section">
-                                <a className="button" href="https://github.com/HobbitCodes" target="_blank" rel="noopener noreferrer">View repo</a>
+                                <ul>
+                                    <li>Add Drag and Drop functionality</li>
+                                    <li>Add progress animations</li>
+                                    <li>Finish styling of site</li>
+                                    <li>Need to check if epic already exists</li>
+                                    <li>Need to check if issue already exists under epic</li>
+                                    <li>Buy Domain</li>
+                                    <li>Launch</li>
+                                </ul>
                             </div>
                         </section>
                     </div>
                 )}/>
-
-                <Route path="/CreateProject" render={({history}) => (
-                    <CreateProject onAddPhoto={(addedPost) => {
-                        this.addPhoto(addedPost);
-                        history.push('/');
-                    }}/>
-                )}/>
-            </section>
+            </div>
         );
     }
 }
